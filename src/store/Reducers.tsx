78ac3828a3ecd8e8871413f604fe646102af9d64
vprada25 <v.prada@udla.edu.co>
2022-03-08 19:10:17 -0500

    feat(reducers): configuration reducers

---
+++ src/store/Reducers.tsx
@@ -0,0 +1,24 @@
+import { combineReducers, AnyAction } from '@reduxjs/toolkit'
+import LoginReducer, {
+  KEY_AUTH,
+  logout,
+  initialState,
+} from '../services/auth/authSlice'
+
+const rootAppReducers = combineReducers({
+  [KEY_AUTH]: LoginReducer,
+})
+
+const rootReducer = (state: any, action: AnyAction) => {
+  if (action.type === logout.type) {
+    state = {
+      [KEY_AUTH]: initialState,
+    }
+  }
+  return rootAppReducers(state, action)
+}
+
+export type RootState = ReturnType<typeof rootAppReducers>
+
+export default rootReducer
+
